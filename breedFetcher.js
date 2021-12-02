67ac51bafdb2e9ebf986478367f4623f2602e9cb
nou-ali <noura4403@gmail.com>
2021-12-02 23:36:20 +0000

    fixed bugs

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,18 @@
 const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
-    if (err) {
-      return callback(err); //checking for error
-    } else {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  if (error) {
+      return callback(error,null); //checking for error
+    }  
       const data = JSON.parse(body);
-      if (data[0] === 0) { // does cat exist?
-        return callback(null, "error... breed not found"); // nope cannot find breed
-      } else {
-        return callback(null, data[0]['description']); // yes it does! Breed description follows...
-
-      }
-    }
+      if (!data[0]) { // does cat exist?
+        return callback("error... breed not found"); // nope cannot find breed
+      }  
+        return callback(null, data[0].description); // yes it does! Breed description follows...
 
   });
 };
 
 module.exports = { fetchBreedDescription };
+
