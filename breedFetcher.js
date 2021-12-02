e4d0791e0357c39f0d83d2f09c3474e21e9d81d3
nou-ali <noura4403@gmail.com>
2021-12-02 19:44:52 +0000

    able to request data

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,6 +4,11 @@ const args = process.argv.slice(2); // for command line arguments
 request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (err, response, body) => {
   if (err) {
     console.log(JSON.parse(err))
-  } 
+  } if (body === "[]") {
+    console.log("error...not able to provide description of cat")
+  } else {
+    const data = JSON.parse(body);
+    console.log('data: ', data[0]['description']);
+  }
 });
 
