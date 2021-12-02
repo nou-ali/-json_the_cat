45e97d5f671d0a5b597b9c6df40fb2621b83ef69
nou-ali <noura4403@gmail.com>
2021-12-02 18:34:39 +0000

    started request

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,3 +2,11 @@ const request = require('request');
 //const fetch = require("node-fetch");
 
 let descriptionUrl = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian';
+
+//http request
+request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
+      if (err) {
+        console.log(err) 
+      }
+
+    });
