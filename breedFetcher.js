fe6ee974e41db7c03ae7ef7fe6d2e3f885e23b5e
nou-ali <noura4403@gmail.com>
2021-12-02 21:40:10 +0000

    finally got function to run with callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,20 @@
 const request = require('request');
-const args = process.argv.slice(2); // for command line arguments
 
 const fetchBreedDescription = function(breedName, callback) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (err, response, body) => {
-  if (err) {
-    console.log(JSON.parse(err));
-  } if (body === "[]") {
-    console.log("error...not able to provide description of cat");
-  } else {
-    const data = JSON.parse(body);
-    console.log('data: ', data[0]['description']);
-  }
-});
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
+    if (err) {
+      return callback(err); //checking for error
+    } else {
+      const data = JSON.parse(body);
+      if (data[0] === 0) { // does cat exist?
+        return callback(null, "error... breed not found"); // nope cannot find breed
+      } else {
+        return callback(null, data[0]['description']); // yes it does! Breed description follows...
+
+      }
+    }
+
+  });
 };
 
-
+module.exports = { fetchBreedDescription };
